b67e65ee4c2d90d716d7c11080039c1e9a6e3f34
andhint <andrew.hintermeier@gmail.com>
2016-11-08 23:32:53 -0500

    Cleaned up/commented script
    Discretizing now works appropriately
    Plots discretized data using log scale

--- leagueDataProcessing.R
+++ leagueDataProcessing.R
@@ -1,12 +1,32 @@
-cat("\014") 
+cat("\014")
 rm(list=ls())
 
 setwd("C:/Users/Andrew/Documents/Projects/RiotAPI/LeagueOfLegendsHeatMap")
-getwd();
-df <- read.table("testData.txt", sep = ",", col.names = c('X' , 'Y'))
+df <- read.table("heatMapData.txt", sep = ",", col.names = c('X' , 'Y'))
 
-processed <- data.frame(matrix(0, 128, 128))
+library("entropy")
 
-for (row in df[1]){
-  print(row)
-}
+# seperate X and Y values for discretizing function
+xValues <- df$X
+yValues <- df$Y
+
+# discretize values and transform to dataframe
+binnedData <- discretize2d(xValues, yValues, numBins1 = 128, numBins2 = 128, r1 = c(-120,14780), r2 = c(-120,14980))
+binnedDataDF <- as.data.frame(binnedData)
+colnames(binnedDataDF) <- c('X', 'Y', 'freq')
+
+# create color palette
+library(RColorBrewer)
+rf <- colorRampPalette(rev(brewer.pal(11,'Spectral')))
+r <- rf(32)
+
+# plot of all point
+library("ggplot2")
+p <- (ggplot(df, aes(x=xValues, y=yValues)) + 
+        geom_point(shape=1))
+print(p)
+
+# plot discretized data using log scale
+p2 <- ggplot(df, aes(x=xValues, y=yValues)) + stat_bin2d(bins=128) + scale_fill_gradientn(colors=r, trans='log') + coord_fixed()
+print(p2)
+print('alldone')
